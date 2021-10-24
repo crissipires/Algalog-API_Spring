c82df6b6bfde17efffc1dd9ab81087340186f597
crissipires <crissiano_pire@hotmail.com>
2021-10-23 22:09:05 -0300

    :sparkles: implements entrega

--- src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
+++ src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
@@ -2,6 +2,7 @@ package com.algaworks.algalog.domain.service;
 
 import com.algaworks.algalog.domain.exception.NegocioException;
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class CatalogoClienteService {
 
     private ClienteRepository clienteRepository;
+
+    public Cliente buscar(Long clienteId){
+        return clienteRepository.findById(clienteId)
+                .orElseThrow(() -> new NegocioException("Cliente não encontrado!"));
+
+    }
 
     @Transactional
     public Cliente salvar(Cliente cliente){
